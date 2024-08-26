3dbb8a07e2568a69f92f21c032be3689e5800cd0
lbucess-rage <rage@lbucess.com>
2024-08-26 15:57:05 +0900

    setHeader 추가

--- apps/viewer/src/pages/api/[...trpc].ts
+++ apps/viewer/src/pages/api/[...trpc].ts
@@ -8,6 +8,7 @@ import { createContext } from '@/helpers/server/context'
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   await cors(req, res)
 
+  res.setHeader('Access-Control-Allow-Origin', '*')
   console.log(`API ${req.method} ${req.url}`)
   return createOpenApiNextHandler({
     router: appRouter,
